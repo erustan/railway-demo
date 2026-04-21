31df393e2ae69a7308798277bc6a1ba0d6f92fea
erustan <131688989+erustan@users.noreply.github.com>
2026-04-22 05:34:22 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
-
 @app.route('/')
-def home():
-    return '<h1>Привет из Railway!</h1>'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+def home():
+
+    return '''
+
+    <h1>🚀 Демо Railway Деплоймента</h1>
+
+    <p>Задеплоено автоматически из GitHub!</p>
+
+    <p>Каждый git push запускает автоматический деплой ✨</p>
+
+    '''
