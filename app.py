2de6f03a922bb1b96bd2396e56695a602e784e93
erustan <131688989+erustan@users.noreply.github.com>
2026-04-22 05:49:55 +0600

    Update от эрустана app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Привет из Railway!</h1>'
+    return '<h1>Привет от Эрустана из Railway!</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
